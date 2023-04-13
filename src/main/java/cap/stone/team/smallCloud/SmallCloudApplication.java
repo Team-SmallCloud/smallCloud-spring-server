f3c99b1ec62a66c6d1e8261687566c169a0daea3
이민욱 <lmo9903@gmail.com>
2023-04-14 00:06:04 +0900

    [init] small cloud 서버 파일 기본 세팅

---
+++ src/main/java/cap/stone/team/smallCloud/SmallCloudApplication.java
@@ -0,0 +1,13 @@
+package cap.stone.team.smallCloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmallCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmallCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cap/stone/team/smallCloud/SmallCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package cap.stone.team.smallCloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmallCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
